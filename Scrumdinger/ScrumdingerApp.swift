79c152bad50c6f142956bbd13d2eaf0646988919
Arie Brainware <publicu001@protonmail.com>
2023-09-11 14:23:55 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Arie Brainware on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
