2e6a3057e79da5a82f81e3f26e46bcfb2319c4a5
Arie Brainware <publicu001@protonmail.com>
2023-09-25 10:51:06 +0700

    Add a theme view

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Arie Brainware on 25/09/23.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
